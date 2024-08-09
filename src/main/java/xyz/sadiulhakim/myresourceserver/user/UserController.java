4a92aa684e01b1109ff474fe4bd619e49736dd85
Sadiul Hakim <sadiulhakim@gmail.com>
2024-08-10 00:16:25 +0600

    Start

--- src/main/java/xyz/sadiulhakim/myresourceserver/user/UserController.java
+++ src/main/java/xyz/sadiulhakim/myresourceserver/user/UserController.java
@@ -1,12 +1,13 @@
 package xyz.sadiulhakim.myresourceserver.user;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping("/user")
@@ -14,13 +15,23 @@ public class UserController {
     private static final List<User> users = new ArrayList<>();
 
     static {
-        users.add(new User("Hakim","hakim@gmail.com",21));
-        users.add(new User("Ashik","ashik@gmail.com",30));
-        users.add(new User("Antor","antor@gmail.com",24));
+        users.add(new User("Hakim", "hakim@gmail.com", 21));
+        users.add(new User("Ashik", "ashik@gmail.com", 30));
+        users.add(new User("Antor", "antor@gmail.com", 24));
     }
 
     @GetMapping("/")
-    public ResponseEntity<?> getAll(){
+    public ResponseEntity<?> getAll() {
         return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/authenticated")
+    public ResponseEntity<?> authenticated(@AuthenticationPrincipal Jwt jwt) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("token",jwt.getTokenValue());
+        data.put("claims",jwt.getClaims());
+        data.put("headers",jwt.getHeaders());
+
+        return ResponseEntity.ok(data);
     }
 }
